f0ea796e7fb8b528715618426f5e53cbe44c0171
josephedward <josephedwardwork@gmail.com>
2022-08-22 00:46:49 -0400

    navigation to AWS
    -can reach IQ but need dashboard stable

---
+++ aws/iq.go
@@ -0,0 +1,35 @@
+package aws
+
+import (
+	"iq-bot/core"
+	"github.com/go-rod/rod"
+	"github.com/go-rod/rod/lib/input"
+	// "time"
+	"fmt"
+)
+
+// "div[class^='CopyableInstanceField__Value']")
+
+func NavIq(connect core.Connection) {
+
+	// globalNav-search
+	elem := 	connect.Page.MustWaitLoad().MustElementR("input", "Search").MustInput("iq").MustType(input.Enter)
+	core.Success("elem : ", elem)
+
+	
+	
+	// elem = connect.Page.MustWaitLoad().MustElementR("a","dashboard").MustClick()
+	// core.Success("elem : ", elem)
+	// MustElement("a[href='/work']").MustClick()
+	
+	
+}
+
+
+func Scrape(connect core.Connection) rod.Elements {
+	connect.Page.MustNavigate("https://iq.aws.amazon.com/work/#/requests")
+	elems := connect.Page.MustWaitLoad().MustElements("div[class^='ProjectRailItem__title']")
+	fmt.Println(elems[0].MustText())
+	return elems
+}
+
